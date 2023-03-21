b427f8bf5a424e3f43a9348e5495869d553ab568
storezhang <storezhang@gmail.com>
2023-03-21 18:43:32 +0800

    feat(项目): 初始化

--- lifecycle.go
+++ lifecycle.go
@@ -1,8 +1,19 @@
 package hud
 
+import (
+	"time"
+)
+
 type lifecycle interface {
-	Request()
-	Initiate() (id string)
-	Abort()
+	// Request 请求上传
+	Request() (url string, expired time.Time, err error)
+
+	// Initiate 初始化
+	Initiate() (id string, err error)
+
+	// Abort 取消
+	Abort(id string)
+
+	// Complete 完成
 	Complete()
 }
--- upload_params.go
+++ upload_params.go
@@ -1,9 +1,40 @@
 package hud
 
+import (
+	"io"
+	"os"
+)
+
 type uploadParams struct {
-	target any
+	target io.ReadSeeker
 }
 
 func newUploadParams() *uploadParams {
 	return &uploadParams{}
+}
+
+func (up *uploadParams) bytes(params *multipartParams, part int64) (parts int, err error) {
+
+}
+
+func (up *uploadParams) parts(params *multipartParams) (parts int64, err error) {
+	if size, se := up.size(); nil != se {
+		err = se
+	} else {
+		parts = size / params.size.Byte()
+	}
+
+	return
+}
+
+func (up *uploadParams) size() (size int64, err error) {
+	if bytes, bok := up.target.([]byte); bok {
+		size = int64(len(bytes))
+	} else if info, se := os.Stat(up.target.(string)); nil != se {
+		err = se
+	} else {
+		size = info.Size()
+	}
+
+	return
 }
